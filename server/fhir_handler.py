4c31ab5fca3628fe052d2fdaca731cef43308e96
s-mbk <somtochukwumbuko@gmail.com>
2024-06-11 00:35:55 +0200

    modified fhir_handler.py

--- server/fhir_handler.py
+++ server/fhir_handler.py
@@ -1,16 +1,13 @@
 # FHIR HANDLER
 
 from fhir.resources.patient import Patient
-from fhir.resources.bundle import Bundle
-import json
+from pydantic import ValidationError
 
-def validate_fhir_data(fhir_json):
+def validate_fhir_data(data):
     try:
-        # Parse FHIR JSON to ensure it's a valid FHIR resource
-        resource = json.loads(fhir_json)
-        
-        # Here we're assuming the resource is a Patient, modify as necessary for other resource types
-        patient = Patient(**resource)
-        return True, "Valid FHIR Data"
+        patient = Patient.parse_raw(data)
+        return True, "FHIR data is valid"
+    except ValidationError as ve:
+        return False, f"Invalid FHIR Data: {ve}"
     except Exception as e:
-        return False, str(e)
+        return False, f"Invalid FHIR Data: {e}"
